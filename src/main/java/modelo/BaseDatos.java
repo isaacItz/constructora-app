ae6845cb00937e0cb9f726136a1401f4d4599f33
isaacItz <isaac.est.cbtis@gmail.com>
2020-06-29 12:30:38 -0500

    visualizar el rol al crear la conexion

--- src/main/java/modelo/BaseDatos.java
+++ src/main/java/modelo/BaseDatos.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.JOptionPane;
+
 import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 
 public class BaseDatos {
@@ -22,6 +24,7 @@ public class BaseDatos {
 				conexion = DriverManager.getConnection("jdbc:mysql://" + host + port + "/consC?serverTimezone=UTC",
 						user, String.valueOf(password));
 			System.out.println("conexion creada");
+			JOptionPane.showMessageDialog(null, "bienvenido" + getRoll());
 		}
 
 		else {
