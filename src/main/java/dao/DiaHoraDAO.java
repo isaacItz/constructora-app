f87bf67862e29f03faba6bce37bc56738f86b97f
jdtv99 <josedanieltv93@gmail.com>
2020-06-30 21:58:17 -0500

    ingresando

---
+++ src/main/java/dao/DiaHoraDAO.java
@@ -0,0 +1,168 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.DiaHora;
+import modelo.Utileria;
+
+public class DiaHoraDAO extends DAO<DiaHora, Integer>{
+	
+
+	private final String INSERT = "INSERT INTO diahora (dia_diahor ,he_diahor ,hs_diahor,cve_hor ) VALUES (?, ?, ?, ?)";
+	private final String MODIFICAR = "UPDATE diahora SET dia_diahor= ?, heo_diahor= ? WHERE cve_hor = ?";
+	private final String OBTENER_TODOS = "SELECT * FROM diahora";
+	private final String OBTENER_UNO = "SELECT * FROM diahora WHERE cve_hor = ?";
+	private final String BUSCAR = "SELECT * FROM diahora WHERE cve_hor = ? ";
+	private final String ELIMINAR = "DELETE FROM trabajadorcon WHERE cve_hor = ?";
+
+	
+	protected DiaHoraDAO(Connection con) {
+		super(con);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insertar(DiaHora t) throws SQLException {
+		try {
+			stat = con.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+			stat.setInt(1, t.getDia());
+			stat.setTime(2,Utileria.getTime(t.getHoren()));
+			stat.setTime(3,Utileria.getTime(t.getHorsal()) );
+			stat.setInt(4, t.getCveHor());
+			
+			if (stat.executeUpdate() == 0) {
+				throw new SQLException("horario ya Registrado");
+			} else {
+				set = stat.getGeneratedKeys();
+				if (set.next()) {
+					t.setCve(set.getInt(1));
+				} else {
+					throw new SQLException("No se ha Podico Asignar la PK");
+				}
+			}
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			cerrarSt();
+			cerrarRs();
+		}
+	}
+
+	@Override
+	public void modificar(DiaHora t) throws SQLException {
+		try {
+			stat = con.prepareStatement(MODIFICAR);
+			stat.setInt(1, t.getDia());
+			stat.setTime(2,Utileria.getTime(t.getHoren()));
+			stat.setTime(3,Utileria.getTime(t.getHorsal()) );
+			
+			if (stat.executeUpdate() == 0) {
+				throw new SQLException("horario ya Registrado");
+			}
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			cerrarSt();
+		}		
+	}
+	@Override
+	public void eliminar(DiaHora t) throws SQLException {
+		try {
+			stat = con.prepareStatement(ELIMINAR);
+			stat.setInt(1, t.getCve());
+			if (stat.executeUpdate() == 0) {
+				throw new SQLException("no se ha eliminado verificar clave");
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			cerrarSt();
+		}	
+	}
+	//cve_diahor dia_diahor he_diahor  hs_diahor   cve_hor
+
+	@Override
+	public DiaHora obtener(Integer clave) throws SQLException {
+		DiaHora dh= null;
+		
+		try {
+			stat = con.prepareStatement(OBTENER_UNO);
+			stat.setInt(1, clave);
+			set = stat.executeQuery();
+			if (set.next()) {
+				dh = convertir(set);
+			} else {
+				throw new SQLException("No Existe el horario");
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			cerrarRs();
+			cerrarSt();
+		}
+		
+		return dh;
+	}
+
+	@Override
+	public DiaHora buscar(DiaHora objeto) throws SQLException {
+		DiaHora dh=null;
+		
+		try {
+			stat = con.prepareStatement(BUSCAR);
+			stat.setInt(1, objeto.getCveHor());
+			set =stat.executeQuery();
+			if (set.next()) {
+				dh = convertir(set);
+			} else {
+				throw new SQLException("No Existe el horario");
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			cerrarRs();
+			cerrarSt();
+		}
+		
+		return dh;
+	}
+
+	@Override
+	public List<DiaHora> obtenerTodos() {
+		List<DiaHora> dh= new ArrayList<>();
+		try {
+			stat = con.prepareStatement(OBTENER_TODOS);
+			set = stat.executeQuery();
+			while (set.next()) {
+				dh.add(convertir(set));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			cerrarRs();
+			cerrarSt();
+		}
+		return dh;
+	}
+	//cve_diahor dia_diahor he_diahor  hs_diahor   cve_hor
+	@Override
+	protected DiaHora convertir(ResultSet set) throws SQLException {
+		DiaHora dh= new DiaHora();
+		dh.setCve(set.getInt(1));
+		dh.setDia(set.getInt("dia_diahor"));
+		dh.setHoren(Utileria.getLocalTime(set.getTime("he_diahor")));
+		dh.setHorsal(Utileria.getLocalTime(set.getTime("hs_diahor")));
+		dh.setCveHor(set.getInt("cve_hor"));
+		
+		
+		return dh;
+	}
+	
+
+}
---
+++ src/main/java/dao/HorarioDAO.java
@@ -0,0 +1,159 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Horario;
+import modelo.Utileria;
+
+public class HorarioDAO  extends DAO<Horario, Integer>{
+	
+	private final String INSERT = "INSERT INTO horario (fecha_hor, tipo_hor) VALUES (?, ?)";
+	private final String MODIFICAR = "UPDATE horario SET fecha_hor= ?, tipo_hor= ? WHERE cve_tracon = ?";
+	private final String OBTENER_TODOS = "SELECT * FROM horario";
+	private final String OBTENER_UNO = "SELECT * FROM horario WHERE cve_tracon = ?";
+	private final String BUSCAR = "SELECT * FROM horario WHERE fecha_hor = ? AND tipo_hor = ? ";
+	private final String ELIMINAR = "DELETE FROM trabajadorcon WHERE cve_tracon = ?";
+
+	protected HorarioDAO(Connection con) {
+		super(con);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insertar(Horario t) throws SQLException {
+		// TODO Auto-generated method stub
+		try {
+			stat = con.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+			stat.setDate(1, Utileria.getDate(t.getFecha()));
+			stat.setString(2, t.getTipo());
+
+			if (stat.executeUpdate() == 0) {
+				throw new SQLException("Usuario ya Registrado");
+			} else {
+				set = stat.getGeneratedKeys();
+				if (set.next()) {
+					t.setCve(set.getInt(1));
+				} else {
+					throw new SQLException("No se ha Podico Asignar la PK");
+				}
+			}
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			cerrarSt();
+			cerrarRs();
+		}
+	}
+
+	@Override
+	public void modificar(Horario t) throws SQLException {
+		try {
+			stat = con.prepareStatement(MODIFICAR);
+			stat.setDate(1, Utileria.getDate(t.getFecha()));
+			stat.setString(2, t.getTipo());
+			
+			if (stat.executeUpdate() == 0) {
+				throw new SQLException("Usuario ya Registrado");
+			}
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			cerrarSt();
+		}
+	}
+
+	@Override
+	public void eliminar(Horario t) throws SQLException {
+		try {
+			stat = con.prepareStatement(ELIMINAR);
+			stat.setInt(1, t.getCve());
+			if (stat.executeUpdate() == 0) {
+				throw new SQLException("no se ha eliminado verificar clave");
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			cerrarSt();
+		}		
+	}
+
+	@Override
+	public Horario obtener(Integer clave) throws SQLException {
+		Horario hr=null;
+		try {
+			stat = con.prepareStatement(OBTENER_UNO);
+			stat.setInt(1, clave);
+			set = stat.executeQuery();
+			if (set.next()) {
+				hr = convertir(set);
+			} else {
+				throw new SQLException("No Existe el Contrato");
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			cerrarRs();
+			cerrarSt();
+		}
+		return hr;
+	}
+
+	@Override
+	public Horario buscar(Horario objeto) throws SQLException {
+		Horario hr=null;
+		
+		try {
+			stat = con.prepareStatement(BUSCAR);
+			stat.setDate(1, Utileria.getDate(objeto.getFecha()));
+			stat.setString(2, objeto.getTipo());set =
+					stat.executeQuery();
+			if (set.next()) {
+				hr = convertir(set);
+			} else {
+				throw new SQLException("No Existe el Contrato");
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			cerrarRs();
+			cerrarSt();
+		}
+		return hr;
+	}
+
+	@Override
+	public List<Horario> obtenerTodos() {
+		List<Horario> hr=new ArrayList<>();
+		
+		try {
+			stat = con.prepareStatement(OBTENER_TODOS);
+			set = stat.executeQuery();
+			while (set.next()) {
+				hr.add(convertir(set));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			cerrarRs();
+			cerrarSt();
+		}
+		return hr;
+	}
+
+	@Override
+	protected Horario convertir(ResultSet set) throws SQLException {
+		Horario hr =new Horario();
+		hr.setCve(set.getInt(1));
+		hr.setFecha(Utileria.getLocalDate(set.getDate("fecha_hor")));
+		hr.setTipo(set.getString("tipo_hor"));
+		
+		return hr;
+	}
+
+}
